3514c44d3d5977d887dd660562c5d054c9049aaf
IlyaPrigogine <prigogine@protonmail.com>
2021-09-09 15:01:43 +0800

    Create 2_deploy.js

---
+++ migrations/2_deploy.js
@@ -0,0 +1,34 @@
+const BigNumber = require('bignumber.js');
+const chalk = require('chalk');
+
+module.exports = async function(deployer,network,accounts) {
+  // Set the Network Settings
+	const IS_MAINNET = (network == 'mainnet');
+	const IS_ROPSTEN = (network == 'ropsten');
+	const IS_DEV = (network == 'development');
+	const IS_GANACHE = (network == 'devganache');
+  	const IS_BSC_TESTNET = (network == 'testnet');
+	const IS_RINKEBY = (network == 'rinkeby');
+
+	// set the deploy address
+	const OWNER = accounts[0];
+	const ADMIN = accounts[1];
+	const account0 = accounts[0];
+	const account1 = accounts[1];
+	const account2 = accounts[2];
+	const account3 = accounts[3];
+	const account4 = accounts[4];
+	const account5 = accounts[5];
+	const account6 = accounts[6];
+	const account7 = accounts[7];
+	console.log(chalk.red.bold(`account0: ${account0}`));
+	console.log(chalk.red.bold(`account1: ${account1}`));
+	console.log(chalk.red.bold(`account2: ${account2}`));
+	console.log(chalk.red.bold(`account3: ${account3}`));
+	console.log(chalk.red.bold(`account4: ${account4}`));
+	console.log(chalk.red.bold(`account5: ${account5}`));
+	console.log(chalk.red.bold(`account6: ${account6}`));
+	console.log(chalk.red.bold(`account7: ${account7}`));
+
+    
+};
